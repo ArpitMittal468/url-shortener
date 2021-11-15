122c9d88144daf4b05446aafda705c502ee79202
Arpit Mittal <youreka468@gmail.com>
2021-11-15 14:49:17 +0530

    Commit

---
+++ src/server.js
@@ -0,0 +1,32 @@
+const http = require('http')
+const socketIo = require('socket.io')
+const { listner } = require('./listner')
+
+const server = http.createServer(listner)
+
+server.listen(
+    8080,
+    "0.0.0.0",
+    () => {
+        console.log(
+            "[Server Started]"
+        )
+    }
+)
+
+let sock = socketIo(server)
+
+sock.on('connection', function(socket){
+   console.log('A user connected');
+   
+   //Whenever someone disconnects this piece of code executed
+   socket.on('disconnect', function () {
+      console.log('A user disconnected');
+   });
+   socket.on('kuchToh', (x)=>{
+       console.log(x)
+       socket.emit('return',{
+           "from" : "backend"
+       })
+   })
+});
